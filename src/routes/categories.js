4efa2b9de570fcac14b82675083f14aa7d04c614
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-11 08:58:26 -0300

    Updates

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,17 +1,12 @@
 const { default: axios } = require('axios');
 const { Router } = require('express');
-const { Product, Category } = require('../db');
+const { Category } = require('../db');
 require('dotenv').config();
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        const data = await Product.findAll({
-            include: {
-                model: Category,
-                attributes: ['name']
-            }
-        })
+        const data = await Category.findAll()
         res.status(200).json(data)
     }
     catch (err) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 const router = Router();
 
 const products = require('./products');
+const categories = require('./categories');
 
 router.use('/products', products);
+router.use('/categories', categories);
 
 
 module.exports = router;
