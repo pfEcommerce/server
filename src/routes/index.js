5171f66f403469a7150ea8fc9f0c040a8c9269db
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-19 12:00:47 -0300

    ewqrwqewqrfwq

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,31 @@
 
 const { User } = require('../db');
 require('dotenv').config();
-const server = require('express').Router();
+const { Router } = require('express');
+const router = Router();
+const cors = require('cors');
+const session = require('express-session');
+
+
+router.use(
+    cors({
+      origin: '*',
+      credentials: true,
+    })
+  );
+  router.use(session(
+    {
+      name: 'sid',
+      secret:'secret', // Debería estar en un archivo de environment
+      resave:false,
+      saveUninitialized:false,
+      cookie:{
+        maxAge: 1000 * 60 * 60 * 2 // Está en milisegundos --> 2hs
+      }
+    }
+  ));
+  
+
 
 const products = require('./products');
 const categories = require('./categories');
@@ -9,11 +33,11 @@ const orders = require('./orders');
 const users = require('./user');
 const opinions = require('./opinions');
 
-server.use('/products', products);
-server.use('/categories', categories);
-server.use('/orders', orders);
-server.use('/users', users);
-server.use('/opinions', opinions);
+router.use('/products', products);
+router.use('/categories', categories);
+router.use('/orders', orders);
+router.use('/users', users);
+router.use('/opinions', opinions);
 
 
-module.exports = server;
+module.exports = router;
