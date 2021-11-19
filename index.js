966386919c4e49707e8d85afc5901229f846da71
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-19 10:58:46 -0300

    efwq

--- index.js
+++ index.js
@@ -12,38 +12,44 @@ const { conn } = require('./src/db.js');
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const local = "http://localhost:3001";
+axios.defaults.baseURL = 'http://localhost:3001';
 
-// const cargaUsers = async () => {
-//     const usuariosPromise = testUsers.map( async (e) => {
-//         const response = await axios.post(`${local}/users/login`, e)
-//     return response
-//     });
-//     Promise.all(usuariosPromise).then(() => {
-//         return console.log("Users cargados")
-//     });
-// };
+if(process.env.PORT == 3001){
+    local = "http://localhost:3001";
+  }else{
+    local = "https://scientiapfdeploy.herokuapp.com";
+  }
 
-// const cargaOrders = async () => {
-//     const ordersPromise = testOrders.map( async (e) => {
-//         const response = await axios.post(`${local}/orders/${e.emailUserTest}`, e)
-//     return response
-//     });
-//     Promise.all(ordersPromise).then(() => {
-//         return console.log("Orders cargados")
-//     });
-// };
+const cargaUsers = async () => {
+    const usuariosPromise = testUsers.map( async (e) => {
+        const response = await axios.post(`${local}/users/login`, e)
+    return response
+    });
+    Promise.all(usuariosPromise).then(() => {
+        return console.log("Users cargados")
+    });
+};
 
-// const allCharges = async  () => {
-//     await allData();
-//     await cargaUsers();
-//     setTimeout(cargaOrders, 1000, "orders");
-// };
+const cargaOrders = async () => {
+    const ordersPromise = testOrders.map( async (e) => {
+        const response = await axios.post(`${local}/orders/${e.emailUserTest}`, e)
+    return response
+    });
+    Promise.all(ordersPromise).then(() => {
+        return console.log("Orders cargados")
+    });
+};
 
-//allCharges();
+const allCharges = async  () => {
+    await allData();
+    await cargaUsers();
+    setTimeout(cargaOrders, 1000, "orders");
+};
+
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
+    allCharges();
     server.listen(process.env.PORT, () => {
       console.log(`%s listening`); // eslint-disable-line no-console
     });
