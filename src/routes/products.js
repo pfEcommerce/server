908be88d2be13658184e843ad3d0aa831465b62c
Santiago Iván Varacca <santiagovaracca@gmail.com>
2021-11-22 01:11:32 -0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -66,7 +66,7 @@ server.post('/', async (req, res) => {
             }
         });
         if (search) {
-            return res.status(200).json(search)
+            return res.status(200).json({error: 'El juego ya existe'})
         };
         const created = await Product.create({
             name,
