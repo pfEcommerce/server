8a4955cbb6ceb42754e1e68c90994502cfaa1b74
cna-cmd <c-n-a@outlook.com>
2021-11-26 23:02:16 -0300

    updateCategory

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -37,4 +37,23 @@ server.post('/addCategory', async (req, res) => {
     }
 });
 
+server.post('/updateCategory', async (req, res) => {
+    try {
+        let { newName, name } = req.body;
+        console.log(newName);
+        let category = await Category.findOne({
+            where: {
+                name: name,
+            },
+        });
+
+        category.name = newName;
+        await category.save();
+
+        res.send("Categoría modificada");
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 module.exports = server;
