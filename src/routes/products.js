1e0f3c2d84fe0380001a5e92cd4789e53f003744
cna-cmd <c-n-a@outlook.com>
2021-11-28 02:45:51 -0300

    products-created

--- src/routes/products.js
+++ src/routes/products.js
@@ -11,19 +11,23 @@ server.get('/', async (req, res) => {
         if (!req.query.name) {
             const data = await Product.findAll({
                 include: [
-                    {model: Category,
-                    attributes: ['name']},
-                    {model: Opinion, 
-                    attributes: ['content', 'revRating','name',"userEmail"]}
+                    {
+                        model: Category,
+                        attributes: ['name']
+                    },
+                    {
+                        model: Opinion,
+                        attributes: ['content', 'revRating', 'name', "userEmail"]
+                    }
                 ]
             })
             res.status(200).json(data)
         }
         else {
             const gameBd = await Product.findAll({
-                where: {name: {[Op.iLike]: '%' + req.query.name + '%'}}
+                where: { name: { [Op.iLike]: '%' + req.query.name + '%' } }
             });
-            let private = gameBd.map(p => 
+            let private = gameBd.map(p =>
                 p.dataValues
             );
             res.status(200).json(private)
@@ -41,13 +45,17 @@ server.get('/:id', async (req, res) => {
         const data = await Product.findOne({
             where: {
                 id: paramsId
-            }, 
+            },
             include: [
-                {model: Category,
-                attributes: ['name']},
-                {model: Opinion, 
-                attributes: ['content', 'revRating','name',"userEmail"]}
-                ]
+                {
+                    model: Category,
+                    attributes: ['name']
+                },
+                {
+                    model: Opinion,
+                    attributes: ['content', 'revRating', 'name', "userEmail"]
+                }
+            ]
         });
         res.status(200).json(data)
     }
@@ -56,6 +64,44 @@ server.get('/:id', async (req, res) => {
     };
 });
 
-
+server.post('/addProduct', async (req, res) => {
+    let {
+        name,
+        price,
+        stock,
+        description,
+        image,
+        categoryName,
+      } = req.body;
+    
+      if(price < 0 ) return res.json({ error: 'El valor de precio no puede ser menor a cero' })
+      if(stock < 0) return res.json({ error: 'El valor de stock no puede ser menor a cero' })
+    
+      try {
+        const [addProduct, created] = await Product.findOrCreate({
+          where: {
+            name,
+            price,
+            stock,
+            description,
+            image
+          },
+        });
+    
+        const findCategories = await Category.findAll({
+          where: {
+            name: {
+              [Op.in]: categoryName,
+            },
+          },
+        });
+    
+        await addProduct.setCategories(findCategories);
+    
+        return res.json(addProduct);
+      } catch (err) {
+        console.log(err);
+      }
+});
 
 module.exports = server;
